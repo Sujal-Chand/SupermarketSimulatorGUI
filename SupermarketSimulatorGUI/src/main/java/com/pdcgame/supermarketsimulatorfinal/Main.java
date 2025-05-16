2edfa726ff5655d37ef077462097d6abbd8c33f7
sujalchand <sujalchand2526@gmail.com>
2025-05-16 20:53:17 +1200

    Testing getting files from previous project

--- SupermarketSimulatorGUI/src/main/java/com/pdcgame/supermarketsimulatorfinal/Main.java
+++ SupermarketSimulatorGUI/src/main/java/com/pdcgame/supermarketsimulatorfinal/Main.java
@@ -1,11 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.pdcgame.supermarketsimulatorfinal;
 
 import java.util.Scanner;
-
 /**
  * @author prisha, sujal
  */
