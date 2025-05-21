2fad5cffbca0271d249d051574088f2b3b351df4
sujalchand <sujalchand2526@gmail.com>
2025-05-21 22:29:13 +1200

    Added hibernate dependencies and resource xml

--- SupermarketSimulatorGUI/src/main/java/com/pdcgame/DBManager.java
+++ SupermarketSimulatorGUI/src/main/java/com/pdcgame/DBManager.java
@@ -15,20 +15,9 @@ import java.sql.SQLException;
 
 public final class DBManager {
 
-    /**
-     * If you try to connect the database on the server, you must start the
-     * server first. Meanwhile, you need to import 'derbyclient.jar' to the
-     * libraries.
-     */
-    //    private static final String URL = "jdbc:derby://localhost:1527/BookStoreDB";
-    /**
-     * If you try to connect the database embedded in the project, you must stop
-     * the server first. Meanwhile, you need to import 'derby.jar' to the
-     * libraries.
-     */
-    private static final String USER_NAME = "pdc"; //your DB username
-    private static final String PASSWORD = "pdc"; //your DB password
-    private static final String URL = "jdbc:derby:database/SuperMarketDB_Ebd; create=true";  //url of the DB host
+    private static final String USER_NAME = "pdc"; // database username
+    private static final String PASSWORD = "pdc"; // database password
+    private static final String URL = "jdbc:derby:database/SuperMarketDB_Ebd; create=true";  // embedded database url
 
     Connection conn;
 
