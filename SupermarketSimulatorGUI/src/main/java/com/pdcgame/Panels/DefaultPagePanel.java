5a4b1703ca507c71226d37a1dd5be07df93f7137
st-21110 <prisha.n9020@gmail.com>
2025-06-13 04:45:51 +1200

    added default panel

---
+++ SupermarketSimulatorGUI/src/main/java/com/pdcgame/Panels/DefaultPagePanel.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pdcgame.Panels;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+/**
+ *
+ * @author prish
+ */
+public class DefaultPagePanel extends JPanel{
+
+    public DefaultPagePanel() 
+    {
+        
+    }
+
+}
