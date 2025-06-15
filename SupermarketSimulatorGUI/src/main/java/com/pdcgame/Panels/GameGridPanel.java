8559b121d44fa59e2dd534e39ec17581c6291853
Sujal-Chand <sujalchand2526@gmail.com>
2025-06-15 19:56:17 +1200

    Working gamegrid

--- SupermarketSimulatorGUI/src/main/java/com/pdcgame/Panels/GameGridPanel.java
+++ SupermarketSimulatorGUI/src/main/java/com/pdcgame/Panels/GameGridPanel.java
@@ -9,30 +9,47 @@ package com.pdcgame.Panels;
  * @author prish
  */
 
+import com.pdcgame.Enums.BoardCell;
+import com.pdcgame.GameState;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class GameGridPanel extends JPanel{
-    
+public class GameGridPanel extends JPanel {
+    private JLabel[][] labelGrid; // store references to labels
+    private JPanel gridPanel;
+    private final int ROWS = 10;
+    private final int COLS = 10;
+    private final Color labelColor = new Color(138, 129, 124);
+    private final Color textColor = new Color(244, 243, 238);
+
     public GameGridPanel() {
-        setLayout(new GridBagLayout()); 
+        setLayout(new GridBagLayout());
         setOpaque(false);
-        setPreferredSize(new Dimension(400, 400)); 
-        JPanel gridPanel = new JPanel(new GridLayout(10, 10, 2, 2));
+        setPreferredSize(new Dimension(400, 400));
+
+        gridPanel = new JPanel(new GridLayout(ROWS, COLS, 2, 2));
         gridPanel.setOpaque(false);
-        gridPanel.setPreferredSize(new Dimension(350, 350)); 
+        gridPanel.setPreferredSize(new Dimension(350, 350));
 
+        labelGrid = new JLabel[COLS][ROWS];
+        initializeGrid();
+
+        add(gridPanel, new GridBagConstraints());
+        updateBoard();
+        javax.swing.Timer timer = new javax.swing.Timer(1000, e -> updateBoard());
+        timer.start();
+    }
+
+    private void initializeGrid() {
         Dimension labelSize = new Dimension(80, 80);
-        Color labelColor = new Color(138, 129, 124);
-        Color textColor = new Color(244, 243, 238);
 
-        for (int i = 1; i <= 100; i++) {
-            int x = (i - 1) % 10;
-            int y = 9 - (i - 1) / 10;
+        for (int i = 0; i < 100; i++) {
+            int x = i % 10;
+            int y = 9 - i / 10;
 
-            JLabel label = new JLabel("S", SwingConstants.CENTER);
+            JLabel label = new JLabel("", SwingConstants.CENTER);
             label.setOpaque(true);
             label.setBackground(labelColor);
             label.setForeground(textColor);
@@ -61,9 +78,22 @@ public class GameGridPanel extends JPanel{
                 }
             });
 
+            labelGrid[x][y] = label;
             gridPanel.add(label);
         }
+    }
 
-        add(gridPanel, new GridBagConstraints());
+    // Call this function to update the grid with new icons
+    public void updateBoard() {
+        BoardCell[][] board = GameState.instance().getBoardManager().get2DBoard();
+
+        for (int x = 0; x < COLS; x++) {
+            for (int y = 0; y < ROWS; y++) {
+                labelGrid[x][y].setText(board[x][y].getIcon());
+            }
+        }
+
+        revalidate();
+        repaint();
     }
 }
